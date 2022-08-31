87eae5d39b0a68101d9d03ec410f78f4480bd680
Alex <alexelkins404@gmail.com>
2022-08-31 09:34:42 -0400

    Created Flywheel Test VexCode Project

---
+++ Robot Code/Flywheel-Testing-2022-08-31T13-32-22/src/main.cpp
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\alexp                                            */
+/*    Created:      Wed Aug 31 2022                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// FlyMotorLeft         motor         1               
+// FlyMotorRight        motor         2               
+// Controller1          controller                    
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
